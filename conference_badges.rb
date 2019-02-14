d9d09d816566d7ecf8f65cf479183170fdaad042
Paul Rosero <paul_rosero@yahoo.com>
2019-02-14 01:19:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(array)
   new_badge = []
   array.each do |attendees|
-    new_badge.push(badge_maker(name))
+    new_badge.push(badge_maker(attendees))
   end
     new_badge
 end
