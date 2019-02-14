24981398d220e1d2099377c79eeeaefe4d81bed3
Paul Rosero <paul_rosero@yahoo.com>
2019-02-14 00:47:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 def batch_badge_creator(array)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   badges.each do |attendees|
-    "Hello, my name is #{name}."
+    
   end
 end
