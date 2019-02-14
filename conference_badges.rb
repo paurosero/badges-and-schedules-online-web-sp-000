983f40cc00c32c995d5aee61e9491a0367029435
Paul Rosero <paul_rosero@yahoo.com>
2019-02-14 02:21:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_badge = []
+  array.each do |attendees|
+    new_badge.push(badge_maker(attendees))
+  end
+    new_badge
+end
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index {|array, attendees|}
+    new_array
+  end
+  new_array
+end
+
+def printer(attendees)
+
+end
